dca38e6e7d0a3a9172d7076ee1496a2b1823fad1
Hessa <hessafahad54@gmail.com>
2022-08-05 23:01:30 +0300

    Upload The Assignments

---
+++ Assignment-3/Assignment-3/main.swift
@@ -0,0 +1,9 @@
+import Foundation
+
+print("please enter the height of the rectangle:")
+let num1 = Int(readLine() ?? "") ?? 1  /* The initial value = 1 if the user didn't enter anything */
+
+print("please enter the width of the rectangle:")
+let num2 = Int(readLine() ?? "") ?? 1  /* The initial value = 1 if the user didn't enter anything */
+
+print("The calculated area of the rectangle is: \(num1 * num2)")
