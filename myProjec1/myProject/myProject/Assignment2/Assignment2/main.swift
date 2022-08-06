4e949ae2de50492ae8f3c2cbcc7ef3441f54c180
طيُف <taif..@MBPalkhasbtyuf.lan>
2022-08-06 21:45:06 +0300

    Taif Alhafi

---
+++ myProjec1/myProject/myProject/Assignment2/Assignment2/main.swift
@@ -0,0 +1,9 @@
+import Foundation
+
+print("please insert the seconds:")
+
+var seconds = Float(readLine()!)
+
+var minute = seconds! / 60
+
+print("\(seconds!) seconds is \(minute) minute")
---
+++ myProjec1/myProject/myProject/Assignment3/Assignment3/main.swift
@@ -0,0 +1,13 @@
+print("please enter the height of the rectangle:")
+var Rectheight = Int(readLine()!)!
+
+print("please enter the width of the rectangle:")
+var Rectwidth = Int(readLine()!)!
+
+var perimeterOfRectangle = (Rectheight * Rectwidth)
+
+
+print("enter Height:" , Rectheight )
+print("enter width:" , Rectwidth )
+print("The calculated area of the rectangle is:" , perimeterOfRectangle )
+
