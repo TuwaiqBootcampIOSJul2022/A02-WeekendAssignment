2ee9cf7ef9b2219b89a38a311190ecaeae92c909
Farah Alyousef <farahalyousef@MacBook-Pro-alkhas-b-Farah.local>
2022-08-06 07:05:19 +0300

    Farah-Alyousef

---
+++ Assignment 1.playground/Contents.swift
@@ -0,0 +1,12 @@
+var firstNumber = 15
+var secondNumber = 10
+
+let z = firstNumber + secondNumber
+let y = firstNumber - secondNumber
+let n = firstNumber * secondNumber
+let f = firstNumber / secondNumber
+
+print("15 + 10 =",z)
+print("15 - 10 =",y)
+print("15 * 10 =",n)
+print("15 / 10 =",f)
---
+++ Assignment 2.playground/Contents.swift
@@ -0,0 +1,4 @@
+var Seconds = 120
+if Seconds == 120 {
+    print("120 seconds is 2 minute")
+}
