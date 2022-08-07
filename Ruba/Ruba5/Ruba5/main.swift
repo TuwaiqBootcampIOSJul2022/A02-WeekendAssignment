64d9d19623f475f512dcd3ea57a29bd72628cc64
Mansour Al-Dossery <MansourN@Mansours-MacBook-Pro.local>
2022-08-07 09:21:28 +0300

    my project ruba

---
+++ Ruba/Ruba5/Ruba5/main.swift
@@ -0,0 +1,8 @@
+print ("Please enter the height of thr rectangle:")
+var height = Int(readLine()!)!
+print ("Please enter the width of thr rectangle:")
+var width = Int(readLine()!)!
+
+var calculatRectangle = (height * width)
+print ("The calculated area of the rectangle is:",calculatRectangle)
+
